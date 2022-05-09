eb6cdd6d828e68ecbd69edcfb0c437b1e6cf6cc9
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-05-09 09:05:19 +0900

    Initial Commit

---
+++ Shared/InsureLandingPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  InsureLandingPageApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/05/09.
+//
+
+import SwiftUI
+
+@main
+struct InsureLandingPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
